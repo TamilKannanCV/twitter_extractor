695f7a8a36084ad6c3c9ee3263f64e33a1eb5319
TamilKannan-personal <tamilkk100@gmail.com>
2021-10-28 18:11:17 +0530

    updated

--- lib/src/download_action.dart
+++
@@ -1,4 +0,0 @@
-abstract class DownloadAction {
-  static const DOWNLOAD_COMPLETE = "0";
-  static const NOTIFICATION_CLICKED = "1";
-}
--- lib/twitter_extractor.dart
+++ lib/twitter_extractor.dart
@@ -1,4 +1,3 @@
 export 'src/twitter_extractor.dart';
 export 'src/models/twitter.dart';
 export 'src/models/video.dart';
-export 'src/download_action.dart';
